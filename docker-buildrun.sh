b5e5194285378e650ed8c1cd22f85955ec762e11
kenmac <ken.macpherson@philips.com>
2021-01-27 12:06:12 -0500

    start script build step if

--- docker-buildrun.sh
+++ docker-buildrun.sh
@@ -13,7 +13,22 @@ if [ -z ${GOOGLE_MAP_KEY} ];
     echo "GOOGLE_MAP_KEY is unset, export GOOGLE_MAP_KEY= "
     exit 1;
 fi
-docker-compose up --build -d 
+
+
+# root@kubnodejs:/votersmap# docker container ls 
+# CONTAINER ID        IMAGE                   COMMAND                  CREATED             STATUS              PORTS                      NAMES
+# 55e4c87446ca        votersmap_vuejswebapp   "docker-entrypoint.s…"   27 hours ago        Up 27 hours         0.0.0.0:8080->8080/tcp     votersmap_vuejswebapp_1
+# b9b8b241fefc        votersmap_smstest       "docker-entrypoint.s…"   27 hours ago        Up 23 hours         0.0.0.0:3000->3000/tcp     votersmap_smstest_1
+# 60b77ab92e50        mongo:4.4.3             "docker-entrypoint.s…"   27 hours ago        Up 27 hours         0.0.0.0:27017->27017/tcp   mongo
+
+export vm_container=$(docker inspect --format="{{.Id}}" votersmap_smstest_1);
+echo "vm_container = $vm_container"
+if [ -z "$(docker inspect --format="{{.Id}}" votersmap_smstest_1)" ]; 
+    then 
+    echo "container $vm_container doesn't exist starting up docker compose ";
+    docker-compose up --build -d 
+fi
+
 sed -i "s/GOOGLE_MAPS_API_KEY_REPLACE/$GOOGLE_MAPS_API_KEY/g" usermap/src/* 
 # sed -i "s/GOOGLE_MAPS_API_KEY_REPLACE/$GOOGLE_MAPS_API_KEY/g" usermap/src/App.vue 
 # sed -i "s/GOOGLE_MAPS_API_KEY_REPLACE/$GOOGLE_MAPS_API_KEY/g" usermap/dist/js/* 
@@ -24,7 +39,8 @@ sed -i "s/GOOGLE_MAPS_API_KEY_REPLACE/$GOOGLE_MAPS_API_KEY/g" usermap/dist/js/*
 npm run build
 ## docker inspect --format="{{.Id}}" votersmap_smstest_1
 docker container restart $(docker inspect --format="{{.Id}}" votersmap_smstest_1)
+sleep 2
 docker container ls
-docker container restart 
+
 
 exit 0
